2c2a3dae024d93f7b4eb140c588d3828e1622dd0
fyzenon <94575414+fyzenon@users.noreply.github.com>
2026-09-05 15:48:19 -0500

    Remove brand property from car objects

--- data.js
+++ data.js
@@ -4,14 +4,12 @@ window.designs = [
         make: "Toyota",
         model: "GR Supra",
         name: "Colorful Implosion",
-        brand: "Toyota",
         tags: ["RWD", "Anime", "Rare"],
         code: "631-187-591",
         thumbnail: "https://i.ibb.co/4wbwqnjJ/A1-F92819-7715-44-A6-87-CF-D0551-C42-EFA5.png"
     },
     {
         name: "RE:Zero Ram",
-        brand: "BMW",
         tags: ["Anime", "RWD", "Epic"],
         code: "819-669-727",
         thumbnail: "https://i.ibb.co/nMcn7kVj/3-E5851-FB-ADDE-43-F0-8-C0-D-E9-AAA7-F7-FD76.png"
@@ -21,7 +19,6 @@ window.designs = [
         make: "Dodge",
         model: "Challenger Super Stock",
         name: "ZZZ",
-        brand: "Dodge",
         tags: ["Anime", "RWD", "Rare"],
         code: "699-2780772",
         thumbnail: "https://i.ibb.co/4RT8p160/B21-BAD25-56-F6-4-FDF-9-D2-C-7-DE9-BBC80-AFC.png"
