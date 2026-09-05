4c9c9ae71da748689046528ca8db22f83e0211ea
fyzenon <94575414+fyzenon@users.noreply.github.com>
2026-09-05 15:58:05 -0500

    Added bmw data back

--- data.js
+++ data.js
@@ -10,6 +10,9 @@ window.designs = [
     },
     {
         name: "RE:Zero Ram",
+        year: 2020,
+        make: "BMW",
+        model: "Competition Coupé",
         tags: ["Anime", "RWD", "Epic"],
         code: "819-669-727",
         thumbnail: "https://i.ibb.co/nMcn7kVj/3-E5851-FB-ADDE-43-F0-8-C0-D-E9-AAA7-F7-FD76.png"
@@ -20,7 +23,7 @@ window.designs = [
         model: "Challenger Super Stock",
         name: "ZZZ",
         tags: ["Anime", "RWD", "Rare"],
-        code: "699-2780772",
+        code: "699-278-772",
         thumbnail: "https://i.ibb.co/4RT8p160/B21-BAD25-56-F6-4-FDF-9-D2-C-7-DE9-BBC80-AFC.png"
     }
 ];
