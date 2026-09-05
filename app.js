9bc56ba01151c139a8050ac11b5dc11769cf4d64
fyzenon <94575414+fyzenon@users.noreply.github.com>
2026-09-05 14:52:15 -0500

    Fix image

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const designs = [
     {
-        name: "Ferrari F40",
-        brand: "Ferrari",
-        tags: ["Classic", "Red"],
-        code: "123-456-789",
-        thumbnail: "artifacts/imgs/img_1788487621849.png"
+        name: "Colorful Implosion",
+        brand: "Toyota",
+        tags: ["RWD", "Anime", "Rare"],
+        code: "631-187-591",
+        thumbnail: "https://i.ibb.co/4wbwqnjJ/A1-F92819-7715-44-A6-87-CF-D0551-C42-EFA5.png"
     },
     {
-        name: "Porsche 911 GT3",
-        brand: "Porsche",
-        tags: ["Modern", "Track"],
-        code: "654-321-987",
-        thumbnail: "artifacts/imgs/img_1788487621849.png"
+        name: "RE:Zero Ram",
+        brand: "BMW",
+        tags: ["Anime", "RWD", "Epic"],
+        code: "819-669-727",
+        thumbnail: "https://i.ibb.co/nMcn7kVj/3-E5851-FB-ADDE-43-F0-8-C0-D-E9-AAA7-F7-FD76.png"
     },
     {
-        name: "McLaren 720S",
-        brand: "McLaren",
-        tags: ["Modern", "Neon"],
-        code: "321-654-987",
-        thumbnail: "artifacts/imgs/img_1788487621849.png"
+        name: "ZZZ",
+        brand: "Dodge",
+        tags: ["Anime", "RWD", "Rare"],
+        code: "699-2780772",
+        thumbnail: "https://i.ibb.co/4RT8p160/B21-BAD25-56-F6-4-FDF-9-D2-C-7-DE9-BBC80-AFC.png"
     }
 ];
 
