e2be7323fbc35d21a7e4e0e9411facf7dc198787
fyzenon <94575414+fyzenon@users.noreply.github.com>
2026-09-06 14:53:01 -0500

    new: fluorite

--- data.js
+++ data.js
@@ -9,12 +9,12 @@ window.designs = [
         thumbnail: "https://i.ibb.co/4wbwqnjJ/A1-F92819-7715-44-A6-87-CF-D0551-C42-EFA5.png"
     },
     {
-        name: "RE:Zero Ram",
+        name: "Anime Girl Pink",
         year: 2020,
         make: "BMW",
         model: "Competition Coupé",
-        tags: ["Anime", "RWD", "Epic"],
-        code: "819-669-727",
+        tags: ["Anime", "Character", "RWD", "Epic"],
+        code: "899-248-243",
         thumbnail: "https://i.ibb.co/nMcn7kVj/3-E5851-FB-ADDE-43-F0-8-C0-D-E9-AAA7-F7-FD76.png"
     },
     {
@@ -34,5 +34,14 @@ window.designs = [
         tags: ["Anime", "AWD", "Epic"],
         code: "104-191-068",
         thumbnail: "https://i.ibb.co/9mTW2Ttt/4517-F12-C-57-C3-407-B-B120-A0-F866-CF740-F.png"
+    },
+    {
+        year: 2022,
+        make: "Acura",
+        model: "NSX Type S",
+        name: "Fluorite",
+        tags: ["Game", "Character", "AWD", "Epic"],
+        code: "154-844-184",
+        thumbnail: "https://i.ibb.co/spFtQTyg/D3-B1-E8-DA-8-E6-A-465-E-A09-A-F68-B9-AD97-E41.png"
     }
 ];
